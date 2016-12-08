a81a0be6d5dcd3392f3337416b13e43c06ff6fcb
kcunanan <kevin.a.cunanan@gmail.com>
2016-12-08 12:12:07 -0800

    changed logo on navbar and added raleway font

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
       $colors = ['#2b3956', '#8d2232', '#3a9ed7', '#434949'];
       $category = ['full stack web development', 'tech', 'sports', 'anime', 'vacation', 'music'];
       $filters = ['web development', 'full stack', 'design', 'handmade', 'mysql5', 'laravel 5', 'ruby on rails', 'jekyll'];
-      $latest = Lookup::where('category', 'blog')->orWhere('category', 'portfolio')->orderBy('date_posted', 'asc')->take(6)->get();
-      $post = Lookup::where('category', 'blog')->orWhere('category', 'post')->orderBy('date_posted', 'asc')->first();
+      $latest = Lookup::where('category', 'blog')->orWhere('category', 'portfolio')->orderBy('date_posted', 'desc')->take(6)->get();
+      $post = Lookup::where('category', 'blog')->orWhere('category', 'post')->orderBy('date_posted', 'desc')->first();
       $categories = Lookup::distinct()->where('category', 'blog')->orWhere('category', 'portfolio')->get(['sub_category']); //will provide distinct
       View::share('category', $category);
       View::share('categories', $categories);
