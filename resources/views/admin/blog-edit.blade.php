acf16149e5ccf79371bd3e2d27a02dcdb58bf71f
kcunanan <kevin.a.cunanan@gmail.com>
2017-08-23 12:33:55 -0700

    added bootstrap to page

--- resources/views/admin/blog-edit.blade.php
+++ resources/views/admin/blog-edit.blade.php
@@ -9,6 +9,7 @@
   <meta property="og:title" content="kcunanan.com - {{ $blog->blog_title }}" />
 @endsection
 @section('stylesheets')
+  <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-col.css') }}">
   <style>
   #portfolio-link, #github-link {
     @if($blog->color) background-color: {{ $blog->color }}; @endif
